d49f3fd8691d355bc1e2ca1563a916ad5f299aa6
Erwin Luz Leon <ierwinx@gmail.com>
2021-03-25 11:55:37 -0600

    se limpia proyecto

--- Navegacion2/ViewController.swift
+++ Navegacion2/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Navegacion2
-//
-//  Created by edeluz on 25/03/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
